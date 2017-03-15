c853f3e704c9aa1a6fbc44462fe86dfae0197c0c
abbath0767 <abbath0767@gmail.com>
2017-03-15 21:53:47 +0300

    init commit

---
+++ app/src/main/java/ng/com/yandextranslate/App.java
@@ -0,0 +1,10 @@
+package ng.com.yandextranslate;
+
+import android.app.Application;
+
+/**
+ * Created by NG on 15.03.17.
+ */
+
+public class App extends Application {
+}
---
+++ app/src/main/java/ng/com/yandextranslate/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package ng.com.yandextranslate.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ng.com.yandextranslate.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
